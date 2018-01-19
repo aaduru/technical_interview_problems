83416939093eda559d277cb8cf0294e27929565f
aaduru <ujwala_aaduru@yahoo.com>
2018-01-18 22:54:28 -0800

    draw board

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -1,5 +1,48 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Hello World!";
+// function myFunction() {
+//     document.getElementById("demo").innerHTML = "Hello World!";
+// }
+//
+// myFunction();
+
+let canvas = document.getElementById("tictactoe");
+let ctx = canvas.getContext('2d');
+let msg = document.getElementById("message");
+let cell = 100;
+canvas.width = canvas.height = 3 * cell;
+
+function draw() {
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+  drawBoard();
+
+  function drawBoard(){
+    ctx.strokeStyle = 'white';
+    ctx.lineWidth = 5;
+
+    ctx.beginPath();
+    ctx.moveTo(cell, 0);
+    ctx.lineTo(cell, canvas.height);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(cell * 2 , 0);
+    ctx.lineTo(cell * 2, canvas.height);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(0, cell);
+    ctx.lineTo(canvas.width, cell);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(0, cell * 2);
+    ctx.lineTo(canvas.width, cell * 2);
+    ctx.stroke();
+
+  }
+  function fillBoard(){
+
+  }
+  requestAnimationFrame(draw);
 }
 
-myFunction();
+draw();
