6c28eed23d3dcf9158cb6cb3beb3f800d301ac71
aaduru <ujwala_aaduru@yahoo.com>
2018-01-24 22:30:48 -0800

    display the memory board

---
+++ memory_game/script.js
@@ -0,0 +1,26 @@
+let memory_array = ['A','A','B','B','C','C','D','D','E','E','F','F','G','G','H','H','I','I','J','J','K','K','L','L'];
+let memory_values = [];
+let memory_tile_ids = [];
+let tiles_flipped = 0;
+
+Array.prototype.memory_tile_shuffle = function() {
+  let i = this.length, j , temp;
+  while(--i > 0){
+    j = Math.floor(Math.random() * (i + 1));
+    temp = this[j];
+    this[j] = this[i];
+    this[i] = temp;
+  }
+}
+
+function newBoard() {
+  tiles_flipped = 0;
+  let output = '';
+  memory_array.memory_tile_shuffle();
+  for(let i = 0; i < memory_array.length; i++) {
+    output += '<div id="tile_'+i+'" onclick="memoryFlipThis(this,\''+memory_array[i]+'\')"></div>';
+  }
+  document.getElementById('memory_board').innerHTML = output;
+}
+
+newBoard();
