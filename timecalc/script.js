551625fb5a3dc2272f0a14e01b72ff1d8a016152
aaduru <ujwala_aaduru@yahoo.com>
2018-01-26 22:06:04 -0800

    end function

--- timecalc/script.js
+++ timecalc/script.js
@@ -1,8 +1,20 @@
 let startTime, endTime;
+
 function start() {
   startTime = new Date();
   console.log("time started");
 };
 
+function end() {
+  endTime = new Date();
+  var timeDiff = endTime - startTime; //in ms
+  // strip the ms
+  //timeDiff /= 1000;
+
+  // get seconds
+  let seconds = Math.round(timeDiff/1000);
+  console.log(seconds + " seconds");
+}
 
 start();
+end();
