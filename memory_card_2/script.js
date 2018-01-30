f577a6724b2a63988e325baf53d506aa8a7cc233
aaduru <ujwala_aaduru@yahoo.com>
2018-01-29 23:03:42 -0800

    timer

--- memory_card_2/script.js
+++ memory_card_2/script.js
@@ -2,4 +2,25 @@ function myFunction(){
 alert("Hello world!");
 }
 
+// function myClock() {
+//     document.getElementById("Clock").innerHTML =
+//     new Date().toLocaleTimeString();
+// }
+
+
+function myClock() {
+    let today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('Clock').innerHTML =
+    h + ":" + m + ":" + s;
+    let t = setTimeout(myClock, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
 //myFunction();
