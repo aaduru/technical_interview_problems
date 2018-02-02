9a1d4738ac0b1aa76c0d3bf963eea57332098964
aaduru <ujwala_aaduru@yahoo.com>
2018-02-01 22:10:29 -0800

    re prompts the question if the user does not enter a value

--- quiz/script.js
+++ quiz/script.js
@@ -1,18 +1,18 @@
 let questions = [
      {
-      prompt: "Why so JavaScript and Java have similar name?\n(a) JavaScript is a stripped-down version of Java\n\ (b) JavaScript's syntax is loosely based on Java's\n(c) They both originated on the island of Java\n (d)None of the above ",
+      prompt: "1. Why so JavaScript and Java have similar name?\n(a) JavaScript is a stripped-down version of Java\n\ (b) JavaScript's syntax is loosely based on Java's\n(c) They both originated on the island of Java\n (d)None of the above ",
       answer: "b"
      },
      {
-      prompt: "When a user views a page containing a JavaScript program, which machine actually executes the script?\n(a) The Users machine running a Web browser\n\ (b) The Web server\n(c) A central machine deep within Netscape s corporate offices\n (d)None of the above",
+      prompt: "2. When a user views a page containing a JavaScript program, which machine actually executes the script?\n(a) The Users machine running a Web browser\n\ (b) The Web server\n(c) A central machine deep within Netscape s corporate offices\n (d)None of the above",
       answer: "a"
      },
      {
-      prompt: "______ JavaScript is also called client-side JavaScript.\n(a) Microsoft\n\ (b) Navigator\n(c) LiveWire\n (d)Native ",
+      prompt: "3. ______ JavaScript is also called client-side JavaScript.\n(a) Microsoft\n\ (b) Navigator\n(c) LiveWire\n (d)Native ",
       answer: "b"
     },
      {
-      prompt: "__________ JavaScript is also called server-side JavaScript.\n(a) Microsoft\n\ (b) Navigator\n(c) LiveWire\n (d)Native ",
+      prompt: "4. __________ JavaScript is also called server-side JavaScript.\n(a) Microsoft\n\ (b) Navigator\n(c) LiveWire\n (d)Native ",
       answer: "c"
      }
 ];
@@ -21,10 +21,18 @@ let score = 0;
 function myQuiz() {
   for(let i = 0; i < questions.length; i++){
     let response = window.prompt(questions[i].prompt);
+
+    while (response == '' ) {
+
+      alert("enter a response")
+      response = window.prompt(questions[i].prompt);
+      break;
+    }
     if(response == questions[i].answer){
       score++;
       alert("Correct!");
-    } else {
+    }
+    else {
       alert("WRONG!");
     }
   }
