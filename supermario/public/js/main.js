1704df3be17a586dc4c43abf39caf8575cf8011d
aaduru <ujwala_aaduru@yahoo.com>
2018-02-08 23:57:03 -0800

    add super mario tile set

--- supermario/public/js/main.js
+++ supermario/public/js/main.js
@@ -1,4 +1,24 @@
+function loadImage(url) {
+  return new Promise(resolve => {
+    const image = new Image();
+    image.addEventListener('load', () => {
+      resolve(image);
+    });
+    image.src = url;
+  });
+}
+
+
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
 context.fillRect(0,0,50,50);
+
+// loads the tile set
+loadImage('/img/tiles.png')
+.then(image => {
+  // draws the entire image if give image,0,0
+  // draws the image in smaller size if we give coordinates like image,0,0,16,16
+  // draws only tile if give image,0,0,16,16,32,32,0,0
+  context.drawImage(image,0,0);
+});
