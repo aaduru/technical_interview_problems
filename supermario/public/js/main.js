63559814181c3cbe96e54911ea90d08fe537d4d2
aaduru <ujwala_aaduru@yahoo.com>
2018-02-12 23:15:45 -0800

    image and level loads parallelly

--- supermario/public/js/main.js
+++ supermario/public/js/main.js
@@ -14,19 +14,34 @@ function drawBackground(background, context, sprites) {
     });
 }
 
-
-loadImage('/img/tiles.png')
-.then(image => {
-  console.log('image loaded',image);
-    const sprites = new SpriteSheet(image);
-    sprites.define('ground', 0, 0);
-    sprites.define('sky', 3, 23);
-
-    loadLevel('1-1')
-    .then(level => {
-      console.log('level loaded',level);
-        level.backgrounds.forEach(bg => {
-            drawBackground(bg, context, sprites);
-        });
+function loadBackgroundSprites(){
+  return loadImage('/img/tiles.png')
+  .then(image => {
+  //  console.log('image loaded',image);
+      const sprites = new SpriteSheet(image);
+      sprites.define('ground', 0, 0);
+      sprites.define('sky', 3, 23);
+      return sprites;
     });
+}
+
+// Promise.all([
+//   promiseA,
+//   promiseB
+// ])
+// .then(([
+//   resultA,
+//   resultB
+// ]) => {});
+Promise.all([
+  loadBackgroundSprites(),
+  loadLevel('1-1')
+])
+.then(([sprites,level]) => {
+  //console.log('level loaded',level);
+  level.backgrounds.forEach(bg => {
+    drawBackground(bg, context, sprites);
+  });
 });
+
+// Image and level loads in parallel
