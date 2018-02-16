a4a4ca697ae94d9b898be8d7afe390880884fd2f
aaduru <ujwala_aaduru@yahoo.com>
2018-02-15 22:34:10 -0800

    add alert msg

--- choices/script.js
+++ choices/script.js
@@ -1,1 +1,10 @@
 confirm("I understand confirm!");
+
+var age = prompt("What's your age");
+if (age < 13) {
+    console.log("you are not allowed to play");
+    alert("you cannot play");
+} else {
+   console.log("you can play");
+   alert("you can play");
+}
