fa9af45fb212371333df571ed89cc54c1cfa086c
aaduru <ujwala_aaduru@yahoo.com>
2018-02-16 21:57:30 -0800

    test story

--- choices/script.js
+++ choices/script.js
@@ -8,3 +8,26 @@ if (age < 13) {
    console.log("you can play");
    alert("you can play");
 }
+
+
+// document.body.style.backgroundImage = "url('./img/pic1.jpg')";
+//     document.body.style.backgroundSize = "600px 400px";
+//     document.body.style.backgroundRepeat = "no-repeat";
+// document.body.style.backgroundImage = "url('./img/pic2.jpg')";
+// //document.body.style.backgroundSize = "100% auto";
+// document.body.style.backgroundRepeat = "no-repeat";
+// //document.body.style.backgroundPosition = "top center";
+// document.body.style.backgroundPosition = "absolute";
+
+var el = document.getElementById("main");
+el.style.background = "url('./img/pic2.jpg') no-repeat center center";
+el.style.backgroundSize="100% 100%";
+
+
+console.log("You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'");
+
+console.log("Suddenly, Bieber stops and says, 'Who wants to race me?'");
+
+var userAnswer = "";
+
+userAnswer = prompt("Do you want to race Bieber on stage?");
