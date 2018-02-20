24a5153785a60547e2a2b607d85c93afc05b8f96
aaduru <ujwala_aaduru@yahoo.com>
2018-02-19 23:01:03 -0800

    add background image

--- choices/script.js
+++ choices/script.js
@@ -1,13 +1,13 @@
 confirm("I understand confirm!");
 
-var age = prompt("What's your age");
-if (age < 13) {
-    console.log("you are not allowed to play");
-    alert("you cannot play");
-} else {
-   console.log("you can play");
-   alert("you can play");
-}
+// var age = prompt("What's your age");
+// if (age < 13) {
+//     console.log("you are not allowed to play");
+//     alert("you cannot play");
+// } else {
+//    console.log("you can play");
+//    alert("you can play");
+// }
 
 
 // document.body.style.backgroundImage = "url('./img/pic1.jpg')";
@@ -19,15 +19,15 @@ if (age < 13) {
 // //document.body.style.backgroundPosition = "top center";
 // document.body.style.backgroundPosition = "absolute";
 
-var el = document.getElementById("main");
-el.style.background = "url('./img/pic2.jpg') no-repeat center center";
-el.style.backgroundSize="100% 100%";
-
-
-console.log("You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'");
-
-console.log("Suddenly, Bieber stops and says, 'Who wants to race me?'");
-
-var userAnswer = "";
-
-userAnswer = prompt("Do you want to race Bieber on stage?");
+// var el = document.getElementById("main");
+// el.style.background = "url('./img/pic2.jpg') no-repeat center center";
+// el.style.backgroundSize="100% 100%";
+//
+//
+// console.log("You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'");
+//
+// console.log("Suddenly, Bieber stops and says, 'Who wants to race me?'");
+//
+// var userAnswer = "";
+//
+// userAnswer = prompt("Do you want to race Bieber on stage?");
