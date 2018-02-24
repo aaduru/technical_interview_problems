1c58e90e5ed1bce1355db625c947a4a0779baa0b
aaduru <ujwala_aaduru@yahoo.com>
2018-02-23 22:14:46 -0800

    add onclick event

--- canvas_exercises/script.js
+++ canvas_exercises/script.js
@@ -1,32 +1,71 @@
-const canvas = document.getElementById("myCanvas")
-const context = canvas.getContext("2d")
+var canvas = document.createElement("CANVAS");
+var ctx = canvas.getContext("2d");
+document.body.appendChild(canvas);
 
-//create your shape data in a Path2D object
-const path = new Path2D()
-path.rect(250, 350, 200, 100)
-path.rect(25,72,32,32)
-path.closePath()
+ctx.font = 'bold 10pt Calibri';
+ctx.fillStyle = "yellow";
+ctx.fillText('Hello World!', 150, 100);
 
-//draw your shape data to the context
-context.fillStyle = "#FFFFFF"
-context.fillStyle = "rgba(225,225,225,0.5)"
-context.fill(path)
-context.lineWidth = 2
-context.strokeStyle = "#000000"
-context.stroke(path)
 
-function getXY(canvas, event){ //adjust mouse click to canvas coordinates
-  const rect = canvas.getBoundingClientRect()
-  const y = event.clientY - rect.top
-  const x = event.clientX - rect.left
-  return {x:x, y:y}
+
+canvas.onclick = function(){
+  alert("mouse was clicked");
 }
 
-document.addEventListener("click",  function (e) {
-  const XY = getXY(canvas, e)
-  //use the shape data to determine if there is a collision
-  if(context.isPointInPath(path, XY.x, XY.y)) {
-    // Do Something with the click
-    alert("clicked in rectangle")
-  }
-}, false)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// button using path//
+
+
+
+
+// const canvas = document.getElementById("myCanvas")
+// const context = canvas.getContext("2d")
+//
+// //create your shape data in a Path2D object
+// const path = new Path2D()
+// path.rect(250, 350, 200, 100)
+// path.rect(25,72,32,32)
+// path.closePath()
+//
+// //draw your shape data to the context
+// context.fillStyle = "#FFFFFF"
+// context.fillStyle = "rgba(225,225,225,0.5)"
+// context.fill(path)
+// context.lineWidth = 2
+// context.strokeStyle = "#000000"
+// context.stroke(path)
+//
+// function getXY(canvas, event){ //adjust mouse click to canvas coordinates
+//   const rect = canvas.getBoundingClientRect()
+//   const y = event.clientY - rect.top
+//   const x = event.clientX - rect.left
+//   return {x:x, y:y}
+// }
+//
+// document.addEventListener("click",  function (e) {
+//   const XY = getXY(canvas, e)
+//   //use the shape data to determine if there is a collision
+//   if(context.isPointInPath(path, XY.x, XY.y)) {
+//     // Do Something with the click
+//     alert("clicked in rectangle")
+//   }
+// }, false)
