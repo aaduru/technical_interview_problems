a256c28fad346600f035f00ad3889d3be28e686d
aaduru <ujwala_aaduru@yahoo.com>
2018-03-01 23:20:17 -0800

    rectangle

--- canvas_exercises/script.js
+++ canvas_exercises/script.js
@@ -40,9 +40,48 @@ function openWin2() {
     window.open("test.html", "_blank");
 }
 
+ctx.lineWidth = 4;
+ctx.strokeStyle = "#00ffff";
+ctx.fillStyle = "#abc";
+roundRect(ctx, 10, 10, 100, 50, 10, true);
+ctx.font="10px Georgia";
+ctx.textAlign="center";
+ctx.textBaseline = "middle";
+ctx.fillStyle = "#00ffff";
+var rectHeight = 50;
+var rectWidth = 100;
+var rectX = 10;
+var rectY = 10;
+ctx.fillText("text inside rectangle",rectX+(rectWidth/2),rectY+(rectHeight/2));
 
 
 
+
+function roundRect(ctx, x, y, width, height, radius, fill, stroke) {
+  if (typeof stroke == "undefined" ) {
+    stroke = true;
+  }
+  if (typeof radius === "undefined") {
+    radius = 5;
+  }
+  ctx.beginPath();
+  ctx.moveTo(x + radius, y);
+  ctx.lineTo(x + width - radius, y);
+  ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+  ctx.lineTo(x + width, y + height - radius);
+  ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+  ctx.lineTo(x + radius, y + height);
+  ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+  ctx.lineTo(x, y + radius);
+  ctx.quadraticCurveTo(x, y, x + radius, y);
+  ctx.closePath();
+  if (stroke) {
+    ctx.stroke();
+  }
+  if (fill) {
+    ctx.fill();
+  }
+}
 
 
 
--- choices/img/images_details.md
+++ choices/img/images_details.md
@@ -1,7 +1,9 @@
 grandma image http://www.netart.us/category/events-celebrations/grandparents-day/
 
-pic 1 
+pic 1
 pic 2 https://www.barnesandnoble.com/b/20-off-select-lego-sets/_/N-2me2
 pic 3 https://www.internet4classrooms.com/newsletter/newsletter_december_2017.htm
 pic 4
 pic 5 http://andrewcroke.com/letting-the-player-create-the-story-the-situation/
+
+grandma2 https://www.pinterest.com/brelene/fine-artgrandparents/?lp=true
