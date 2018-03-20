e5762330a767c0c75cf81ad9a7ca053ab126215b
aaduru <ujwala_aaduru@yahoo.com>
2018-03-19 21:08:01 -0700

    add offset

--- tetris/script.js
+++ tetris/script.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d');
 
+context.scale(20,20);
+
 context.fillStyle = "#000";
 context.fillRect(0,0,canvas.width,canvas.height);
 
@@ -11,11 +13,20 @@ const matrix = [
                  [0,1,0],
                ];
 
-matrix.forEach((row,y) => {
-  row.forEach((value, x) => {
-    if (value !== 0) {
-      context.fillStyle="red";
-      context.fillRect(x,y,1,1);
-    }
+
+function drawMatrix(matrix, offset) {
+
+  matrix.forEach((row,y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        context.fillStyle="red";
+        context.fillRect(x + offset.x,
+                         y + offset.y,
+                         1,
+                         1);
+      }
+    });
   });
-});
+}
+
+drawMatrix(matrix, {x:5, y:5});
